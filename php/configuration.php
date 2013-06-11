f7ec78a7409ea52e3dbf4c824f3c69bf6d778039
Nihon Yamori <yamori.jp@gmail.com>
2013-06-11 23:46:32 +0900

    initial

---
+++ php/configuration.php
@@ -0,0 +1,69 @@
+<?php
+class JConfig {
+	public $offline = '0';
+	public $offline_message = 'このサイトはメンテナンスのためサービスを一時停止しています。<br />しばらくしてから再度ご確認ください。';
+	public $display_offline_message = '1';
+	public $offline_image = '';
+	public $sitename = 'OpenShift Joomla';
+	public $editor = 'tinymce';
+	public $captcha = '0';
+	public $list_limit = '20';
+	public $access = '1';
+	public $debug = '0';
+	public $debug_lang = '0';
+	public $dbtype = 'mysqli';
+	public $host = '';
+	public $user = '';
+	public $password = '';
+	public $db = '';
+	public $dbprefix = 'jm3_';
+	public $live_site = '';
+	public $secret = 'KtNokpH0BpJbPE3K';
+	public $gzip = '0';
+	public $error_reporting = 'default';
+	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&amp;keyref=Help{major}{minor}:{keyref}';
+	public $ftp_host = '';
+	public $ftp_port = '';
+	public $ftp_user = '';
+	public $ftp_pass = '';
+	public $ftp_root = '';
+	public $ftp_enable = '';
+	public $offset = 'UTC';
+	public $mailer = 'mail';
+	public $mailfrom = 'uchicrew@gmail.com';
+	public $fromname = 'OpenShift Joomla';
+	public $sendmail = '/usr/sbin/sendmail';
+	public $smtpauth = '0';
+	public $smtpuser = '';
+	public $smtppass = '';
+	public $smtphost = 'localhost';
+	public $smtpsecure = 'none';
+	public $smtpport = '25';
+	public $caching = '0';
+	public $cache_handler = 'file';
+	public $cachetime = '15';
+	public $MetaDesc = '';
+	public $MetaKeys = '';
+	public $MetaTitle = '1';
+	public $MetaAuthor = '1';
+	public $MetaVersion = '0';
+	public $robots = '';
+	public $sef = '1';
+	public $sef_rewrite = '0';
+	public $sef_suffix = '0';
+	public $unicodeslugs = '0';
+	public $feed_limit = '10';
+	public $log_path = '';
+	public $tmp_path = '';
+	public $lifetime = '30';
+	public $session_handler = 'database';
+
+	public function __construct() {
+		$this->host = getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT");
+		$this->user = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+		$this->password = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+		$this->db = getenv("OPENSHIFT_APP_NAME");
+		$this->log_path = getenv("OPENSHIFT_LOG_DIR");
+		$this->tmp_path = getenv("OPENSHIFT_TMP_DIR");
+	}	
+}
